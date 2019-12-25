40051b5da297a5d36c0cf666d28725306076fb0a
Andrew Schwartz <andrew@aschwartz.me>
2019-12-24 18:13:17 -0700

    add website column, pass candidates csv to get_campaign_website script

--- get_campaign_website.py
+++ get_campaign_website.py
@@ -3,9 +3,25 @@ import pdb
 import requests
 from bs4 import BeautifulSoup
 import re
+import pandas as pd
+import sys
+import time
 
 
-candidates = get_all_candidates.get_all_candidates()
+def check_arguments():
+	if len(sys.argv) < 2:
+		print('must specify a candidates csv filename as arg1...exiting')
+		quit(99)
+
+	candidates_filename = sys.argv[1]
+
+	try:
+		candidates = pd.read_csv(candidates_filename, index_col=0)
+	except FileNotFoundError as e:
+		print('the specified csv filename ({0}) does not exist...exiting'.format(candidates_filename))
+		quit(99)
+
+	return candidates
 
 
 def ballotpedia_search(candidate_name):
@@ -24,10 +40,10 @@ def ballotpedia_search(candidate_name):
 
 	response = requests.get('https://ballotpedia.org/wiki/index.php', headers=headers, params=params)
 
-	print(response.status_code)
-
 	soup = BeautifulSoup(response.content, 'html.parser')
+
 	website_tag = soup.find('a', text=re.compile('Campaign website'))
+
 	if website_tag is not None:
 		return website_tag['href']
 	return None
@@ -37,8 +53,32 @@ def check_for_email_form(soup):
 	return soup.find('form').find('input', {'type': 'email'}) is not None
 
 
-for index, row in candidates.head(90).iterrows():
-	print(ballotpedia_search(row['name']))
+def add_website_column(candidates):
+	try:
+		candidates['website']
+	except KeyError as e:
+		# website column does not yet exist
+		candidates.insert(3, 'website', '0')
+		# note: '0' means a website hasn't been checked yet, None means no website has been found
+	return candidates
+
+
+candidates = check_arguments()
+
+candidates = add_website_column(candidates) # adds a website column if one does not already exist
+
+for index, row in candidates.iterrows():
+	print('running', row['name'], '...')
+	if row['website'] is not '0': # this website has already been ID'd
+		continue
+	website = ballotpedia_search(row['name'])
+	candidates.at[index, 'website'] = website
+	if index % 50 == 0 and index != 0:
+		candidates.to_csv(
+			'all_candidates_info_with_websites__{}.csv'.format(index)
+		)
+		print('saved, index:', index)
+		pdb.set_trace()
 
 
 pdb.set_trace()
