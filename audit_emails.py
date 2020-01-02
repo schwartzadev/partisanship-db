884bf56c1b6aefd5c3bcdb282dbaa2f980ff23fb
Andrew Schwartz <andrew@aschwartz.me>
2020-01-01 20:12:32 -0700

    begin code to audit emails by campaign

---
+++ audit_emails.py
@@ -0,0 +1,62 @@
+import imaplib
+import email
+import json
+import pandas as pd
+
+import get_campaign_website
+
+candidates = get_campaign_website.check_arguments()
+
+
+SINCE_DATE_STRING = '01-Dec-2019'
+# SINCE_DATE_STRING = '01-Jan-2020'
+
+with open('user_info.json') as json_file:
+    data = json.load(json_file)
+    GMAIL_USERNAME = data['username'] + '@gmail.com'
+    GMAIL_PASSWORD = data['password']
+
+
+
+def extract_body(payload):
+    if isinstance(payload,str):
+        return payload
+    else:
+        return '\n'.join([extract_body(part.get_payload()) for part in payload])
+
+def connect_to_email():
+    conn = imaplib.IMAP4_SSL("imap.gmail.com", 993)
+    conn.login(GMAIL_USERNAME, GMAIL_PASSWORD)
+    conn.select('"[Gmail]/All Mail"')
+    return conn
+
+def check_campaign_emails_by_id(connection, campaign_id):
+    typ, data = connection.search(
+        None,
+        'TO "partisanshipdatabase+{0}@gmail.com" SINCE "{1}"'.format(
+            campaign_id,
+            SINCE_DATE_STRING
+        )
+    )
+
+    return len(data[0].split()), data[0].split()
+
+def print_email_detail_by_email_id(connection, email_id):
+    typ, msg_data = connection.fetch(email_id, '(RFC822)')
+    for response_part in msg_data:
+        if isinstance(response_part, tuple):
+            msg = email.message_from_bytes(response_part[1])
+            print(msg['subject'], '    ', msg['from'])
+            # payload = msg.get_payload()
+            # body = extract_body(payload)
+            # print(body)
+
+
+conn = connect_to_email()
+count, email_ids = check_campaign_emails_by_id(conn, 'co0605')
+
+try:
+    conn.close()
+except:
+    pass
+conn.logout()
